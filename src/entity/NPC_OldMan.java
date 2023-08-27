31119083bca893c7b8c6c0e1153b83e2b00d7b57
Amritgrg1 <gamrit450@gmail.com>
2023-08-27 15:53:52 +0545

    created Dialogue box

--- src/entity/NPC_OldMan.java
+++ src/entity/NPC_OldMan.java
@@ -12,6 +12,7 @@ public class NPC_OldMan extends Entity {
         speed = 1;
 
         getImage();
+        setDialogue();
     }
     public void getImage(){
 
@@ -24,6 +25,12 @@ public class NPC_OldMan extends Entity {
         right1 = setup("/npc/oldman_right_1");
         right2 = setup("/npc/oldman_right_2");
 
+    }
+    public void setDialogue() {
+        dialogues[0] = "Hello, lad.";
+        dialogues[1] = "So you've come to this island to \nfind the treasure?";
+        dialogues[2] = "I used to be a great wizard but now... \nI'm a bit too old for taking an adventure.";
+        dialogues[3] = "Well, good luck on you.";
     }
     public void setAction() {
 
@@ -48,7 +55,11 @@ public class NPC_OldMan extends Entity {
 
             actionLockCounter = 0;
         }
+    }
+    public void speak() {
 
+        // Do this character specific stuff
+        super.speak();
     }
 
 
