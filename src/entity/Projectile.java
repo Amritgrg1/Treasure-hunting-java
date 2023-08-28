cf040e3f396b06de5f83d45da275d19e08866ebd
ishan058 <ishankhadkaik246@gmail.com>
2023-08-28 17:11:28 +0545

    fonts

---
+++ src/entity/Projectile.java
@@ -0,0 +1,17 @@
+//package entity;
+//
+//import main.Gamepanel;
+//public class Projectile extends Entity{
+//
+//    public Projectile(Gamepanel gp) {
+//        super(gp);
+//    }
+//
+//    public void set() {
+//
+//    }
+//
+//    public void update() {
+//
+//    }
+//}
