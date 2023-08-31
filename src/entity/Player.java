c72c38fb2315bb9673ea068b60bcba7026fe5c55
Amritgrg1 <gamrit450@gmail.com>
2023-08-31 20:16:36 +0545

    eventI handling

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -81,6 +81,11 @@ public class Player extends Entity{
             int npcIndex = gp.cChecker.checkEntity(this, gp.npc);
             interactNPC(npcIndex);
 
+            // CHECK EVENT
+            gp.eHandler.checkEvent();
+
+            gp.keyH.enterPressed = false;
+
             //if collision is false, player can move
             if(collisionOn == false){
                 switch (direction){
@@ -124,7 +129,6 @@ public class Player extends Entity{
                 gp.npc[i].speak();
             }
         }
-        gp.keyH.enterPressed = false;
     }
     public void draw(Graphics2D g2){
         BufferedImage image = null;
